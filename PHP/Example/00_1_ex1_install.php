1f3e32ab61f6bdb4d5fbf03e0c36c1938334117b
ByungjooPark <milkpbj1@naver.com>
2023-03-21 23:38:35 +0900

    0321 php_install udt

--- PHP/Example/00_1_ex1_install.php
+++ PHP/Example/00_1_ex1_install.php
@@ -11,12 +11,15 @@
 
 	1-4. php.ini를 작성
 		- "C:\php8.2.4\"아래의 php.ini-development를 복사하여 "php.ini"로 작성
-		- 198번째 줄 : short_open_tag 설정 (php구분 태그인 <?php ?> 를 <? ?>로 작성 가능)
+		- 198행 : short_open_tag 설정 (php구분 태그인 <?php ?> 를 <? ?>로 작성 가능)
 			short_open_tag = On
-		- 767, 768번째 줄 : 주석해제 및 설정
+		- 767, 768행 : 주석해제 및 설정
 			On windows:
 			extension_dir = "PHP설치경로/ext"
-		- extension 관련 설정 주석해제
+		- 972행 : extension 관련 설정 주석해제 (필요에 따라 주석 제거)
+			extension=mbstring
+		- 977행 : 시간대 설정
+			date.timezone = Asia/Seoul
 	
 	1-5. 환경변수 설정
 		- 윈도우 검색창에 "환경 변수"를 입력해서 "시스템 환경 변수 편집"에 진입
@@ -29,19 +32,44 @@
 		- php 버전이 정상적으로 출력되면 설치완료
 
 2.  Apache 설치
-	1-1. 다운로드 페이지 접속
+	2-1. 다운로드 페이지 접속
 		https://www.apachelounge.com/download/
 
-	1-2. 각 설치 환경에 맞는 파일 다운로드
+	2-2. 각 설치 환경에 맞는 파일 다운로드
 		(실습에서는 "Apache 2.4.56 Win64"를 다운로드)
 	
-	1-3. "C:\Apache24"에 압축 풀기
+	2-3. "C:\Apache24"에 압축 풀기
 		- 최종적으로 conf폴더가 "C:\Apache24"에 배치됩니다.
 	
-	1-4. httpd.conf 설정
+	2-4. httpd.conf 설정
 		- "C:\Apache24\conf\"아래의 httpd.conf를 수정
-		- 37번째 줄 : "Define SRVROOT"를 Apache24를 설치했던 경로로 변경(순서대로 설치했다면 변경 불필요)
-		- 227번째 줄 : "ServerName" 주석 해제
-		- 가장 마지막줄에 아래 설정 추가
-
+		- 37행 : "Define SRVROOT"를 Apache24를 설치했던 경로로 변경(순서대로 설치했다면 변경 불필요)
+		- 227행 : "ServerName" 주석 해제
+		- 285행 : index페이지 설정 (PHP연동 관련 설정)
+			<IfModule dir_module>
+				DirectoryIndex index.php index.html
+			</IfModule>
+		- 가장 마지막줄에 아래 설정 추가 (PHP연동 관련 설정)
+			PHPIniDir "C:/php8"
+			LoadModule php_module "C:/php8/php8apache2_4.dll"
+			AddHandler application/x-httpd-php .php
+			AddType application/x-httpd-php .php .html
 		- 저장
+	
+	2-5. 명령 프롬프트(CMD)를 열고 아래 명령어를 입력
+		- 아파치 httpd.exe 가 있는 위치로 이동
+			위 순서대로 했을 시 경로는 "C:\Apache24\bin\"
+		- httpd -k install 
+		- 설치 완료후 Apache 기동
+			net start apache2.4
+		- 브라우저에서 localhost에 접속
+			"It works!"가 뜨면 Apache설치 완료
+
+	2-6. php 연동 확인
+		- "\Apache24\htodcs"에 phpinfo.php를 배치
+			** phpinfo.php의 내용 **
+				<?php
+					phpinfo();
+				?>
+		- 브라우저에서 localhost/phpinfo.php에 접속
+			php화면이 뜨면 연동 완료
