b478a1491270216e018b454dfcaa1835d08546b7
ByungjooPark <exit>
2023-04-01 16:20:24 +0900

    0401 php edu up

--- PHP/Example/99_1_ex1_edu.php
+++ PHP/Example/99_1_ex1_edu.php
@@ -1,5 +1,7 @@
 <?php
-
+//---------------------------------------------------
+// 보강 1
+//---------------------------------------------------
 // 콘솔에 출력
 //print "프린트";
 
@@ -105,5 +107,238 @@ $i < 0 ? $str = "000" : $str = "111";
 $i = 4;
 $str = "";
 $i % 2 === 0 ? $str = "짝수" : $str = "홀수";
-echo $str;
+//echo $str;
 
+
+//---------------------------------------------------
+// 보강 2
+//---------------------------------------------------
+
+// if문
+$i = 8;
+//if( $i % 2 === 0 )
+//{
+//	echo "짝수";
+//}
+//else
+//{
+//	echo "홀수";
+//}
+
+$i = 1;
+//if( $i === "1" )
+//{
+//	echo "문자열 1입니다.";
+//}
+//else if ( $i === 1 )
+//{
+//	echo "숫자 1입니다.";
+//}
+//else
+//{
+//	echo "1이 아닙니다.";
+//}
+
+
+// 과목의 종류는 국어, 영어, 수학, 과학
+// 평균점수가 60점 이상이고, 각 과목별 점수가 40점 이상 일 때 "합격"을 출력
+// 그 외는 "불합격"을 출력하는 프로그램을 만들어 주세요.
+$sco_kor = 60;
+$sco_eng = 40;
+$sco_meth = 100;
+$sco_sci = 90;
+$sub_avg = ( $sco_kor + $sco_eng + $sco_meth + $sco_sci ) / 4;
+//if(
+//	$sub_avg >= 60 && $sco_kor >= 40 && $sco_eng >= 40
+//	&& $sco_meth >= 40 && $sco_sci >= 40
+//)
+//{
+//	echo "합격";
+//}
+//else
+//{
+//	echo "불합격";
+//}
+
+
+// switch문
+// $str_loc 에 지역명을 저장하고,
+// 서울은 "서울사람", 대구는 "대구사람", 부산은 "부산사람",
+// 그 외는 "타지역사람"을 출력하는 프로그램을 switch문으로 작성해주세요.
+$str_loc = "옥천";
+//switch( $str_loc ) {
+//	case "서울":
+//		echo "서울사람";
+//		break;
+	
+//	case "대구":
+//		echo "대구사람";
+//		break;
+
+//	case "부산":
+//		echo "부산사람";
+//		break;
+	
+//	default:
+//		echo "타지역사람";
+//		break;
+//}
+
+// 반복문 : while, do_while, for, foreach
+$i = 1;
+// while문 : 조건을 체크하고 연산을 실행하는 루프
+//while( $i !== 1 )
+//{
+//	echo $i;
+//}
+
+$i = 1;
+// do-while문 : 연산을 실행하고 조건을 체크하는 루프
+//do
+//{
+//	echo $i;
+//} while( $i !== 1 );
+
+// for문 : 시작과 끝을 우리가 지정해주는 루프(루프의 횟수를 지정가능)
+//for($i = 0; $i < 2; $i++)
+//{
+//	echo "$i \n";
+//}
+
+// 1 ~ 50 까지, 반복문을 이용해서 더하는 프로그램을 만들어 주세요.
+$i = 1;
+$result_sum = 0;
+while( $i <= 50 )
+{
+	$result_sum += $i;
+	++$i;
+}
+//echo $result_sum;
+
+//---------------------------------------------------
+// 보강 3
+//---------------------------------------------------
+//배열
+$arr_i = array(1, 2, 3);
+//echo $arr_i[1];
+
+// 연상배열
+// 키값 : std_no, std_name, std_age, std_gender
+$arr_acc = array(
+	"std_no" => 0
+	,"std_name" => "박병주"
+	,"std_age" => 19
+	,"std_gender" => "남"
+);
+//echo $arr_acc["std_name"];
+
+// 다차원 배열
+$arr_acc = array(
+		"std_no" => 0
+		,"std_name" => "박병주"
+		,"std_age" => 19
+		,"std_gender" => "남"
+		,"std_secret_info"
+			=> array(
+					"std_city_no" => 0716
+					,"std_addr" => "대구 중구 중앙대로"
+				)
+	);
+//echo $arr_acc["std_secret_info"]["std_addr"];
+
+
+// foreach문 : 배열을 루프해서 연산을 하고 싶은 때
+$arr_acc = array(
+	"std_no" => 0
+	,"std_name" => "박병주"
+	,"std_age" => 19
+	,"std_gender" => "남"
+);
+//foreach( $arr_acc as $key => $val )
+//{
+//	echo "$key : $val\n";
+//}
+//foreach( $arr_acc as $val )
+//{
+//	echo "$val\n";
+//}
+
+// foreach를 돌려서 나이만 출력되게 프로그램을 만들어 주세요. 
+// 콘솔에 출력되는 양식 ( 19 )
+$arr_acc = array(
+	"std_no" => 0
+	,"std_name" => "박병주"
+	,"std_age" => 19
+	,"std_gender" => "남"
+);
+foreach($arr_acc as $key => $val )
+{
+	if( $key === "std_age" )
+	{
+		$arr_acc[$key] += 10;
+	}
+}
+
+//print_r($arr_acc);
+
+
+// 함수 : 사용하는 이유는 재사용성 증가, 가독성 증가
+
+//------------------------------
+// 함수명 	: clean_class_room
+// 기능     : 이름과 구역을 받아 청소지정 문자열을 반환
+// 파라미터 : $pram_name  string
+//			 $param_loc  string
+// 리턴     : $result_str  string
+//------------------------------
+function clean_class_room( $pram_name, $param_loc )
+{
+	$result_str = $pram_name."씨, ".$param_loc."을/를 청소해주세요.\n";
+	return $result_str;
+}
+//$result = clean_class_room("봉정", "책상");
+//echo $result;
+//clean_class_room("동호", "책상");
+//clean_class_room("미현", "책상");
+//clean_class_room("수지", "창문");
+//clean_class_room("성엽", "창문");
+//clean_class_room("진영", "창문");
+//clean_class_room("주영", "바닥");
+//clean_class_room("민진", "바닥");
+
+//echo "봉정씨, ";
+//echo "책상을 청소해주세요.\n";
+//echo "동호씨, ";
+//echo "책상을 청소해주세요.\n";
+
+//echo "혁재씨, ";
+//echo "창문을 청소해주세요.\n";
+//echo "진영씨, ";
+//echo "창문을 청소해주세요.\n";
+//echo "성엽씨, ";
+//echo "창문을 청소해주세요.\n";
+
+//echo "주영씨, "; 
+//echo "바닥을 청소해주세요.\n";
+//echo "유정씨, "; 
+//echo "바닥을 청소해주세요.\n";
+
+
+
+//------------------------------
+// 함수명 	: my_sum_all
+// 기능     : 1부터 지정숫자까지의 합을 구해서 리턴
+// 파라미터 : $param_int	int
+// 리턴     : $result_int	int
+//------------------------------
+function my_sum_all( $param_int )
+{
+	$result_int = 0;
+	for( $i = 1; $i <= $param_int; $i++ )
+	{
+		$result_int += $i;
+	}
+	return $result_int;
+}
+
+echo my_sum_all(5);
