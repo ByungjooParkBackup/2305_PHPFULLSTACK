27385552fcd2d890c2a29452d4488bfbd0284073
ByungjooPark <exit>
2023-04-04 12:28:27 +0900

    0404 php edu udt

--- PHP/Example/404_1_ex1_try_catch.php
+++ PHP/Example/404_1_ex1_try_catch.php
@@ -2,18 +2,18 @@
 
 include_once("./12_2_ex2_fnc_db_conn.php");
 
-// try-catch문 : 에러처리를 하기위한 문법
+// try-catch문 : 예외처리를 하기위한 문법
 //try
 //{
 //	// 우리가 실행하고 싶은 소스코드를 작성
 //}
 //catch( Exception $e )
 //{
-//	// 에러가 발생 했을 때 실행되는 소스코드를 작성
+//	// 예외가 발생 했을 때 실행되는 소스코드를 작성
 //}
 //finally
 //{
-//	// 정상처리 또는 에러처리 시에 무조건 실행되는 소스코드를 작성
+//	// 정상처리 또는 예외처리에 관계 없이 무조건 실행되는 소스코드를 작성
 //}
 
 try
@@ -26,7 +26,7 @@ try
 
 	if( count( $result ) === 0 )
 	{
-		// throw Exception : 에러를 강제로 일으키는 구문
+		// throw Exception : 예외를 강제로 일으키는 구문
 		throw new Exception("E99");
 	}
 
