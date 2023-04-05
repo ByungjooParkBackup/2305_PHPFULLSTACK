aad411e9d39980a16e3b5f92ad5a855b261eb992
ByungjooPark <exit>
2023-04-05 18:03:17 +0900

    0405 php edu bj udt

--- PHP/Training/999_1_ex1_blackjack.php
+++ PHP/Training/999_1_ex1_blackjack.php
@@ -12,20 +12,59 @@
 // 4-3. A는 1점 또는 11점 둘 중의 하나로 계산
 //5. 카드의 합이 같으면 다음의 규칙에 따름
 // 5-1. 카드수가 적은 쪽이 승리
-// 5-2. 카드수가 같을경우 스페이드>크로버>다이아>하트 순
+// 5-2. 카드수가 같을경우 비긴다.
 //6. 유저가 카드를 받을 때 딜러는 아래의 규칙을 따른다.
 // 6-1. 딜러는 카드의 합이 17보다 낮을 경우 카드를 더 받음
 // 6-2. 17 이상일 경우는 받지 않는다.
 //7. 1입력 : 카드 더받기, 2입력 : 카드비교, 0입력 : 게임종료
+//8. 한번 사용한 카드는 다시 쓸 수 없다.
 
-while(true) {
-	echo '시작';
-	print "\n";
-	fscanf(STDIN, "%d\n", $input);        
-	if($input === 0) {
-		break;
+class BlackJack
+{
+	private $arr_num;
+	private $arr_shape;
+	private $arr_deck;
+
+	// construct
+	public function __construct()
+	{
+		$this->arr_num = array( "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
+		$this->arr_shape = array("♠", "♣", "◆", "♥");
+		$this->set_deck();
 	}
-	echo $input;
-	print "\n";
+
+	// set
+	private function set_deck()
+	{
+		// 카드 52장 덱에 셋팅
+		foreach( $this->arr_shape as $shape )
+		{
+			foreach( $this->arr_num as $num )
+			{
+				$this->arr_deck[] = $num.$shape;
+			}
+		}
+		// 덱 셔플
+		shuffle( $this->arr_deck );
+	}
+
+	public function debug()
+	{
+		var_dump( $this->arr_deck );
+	}
 }
-echo "끝!\n";
+$obj_bj = new BlackJack();
+$obj_bj->debug();
+
+
+//while(true) {
+//	echo '시작';
+//	print "\n";
+//	fscanf(STDIN, "%d\n", $input);        
+//	if($input === 0) {
+//		break;
+//	}
+//	echo $input;
+//	print "\n";
+//}
+//echo "끝!\n";
